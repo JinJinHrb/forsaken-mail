43e75085fa9685393d766c5adc09861d388b74c7
yamBean <fan.w_2005@163.com>
2019-03-25 22:51:01 +0800

    dEV mailin.js

--- modules/mailin.js
+++ modules/mailin.js
@@ -11,7 +11,16 @@ let config = require(path.join(__dirname, '..', 'config-default.json'));
 mailin.start(config.mailin);
 
 mailin.on('error', function(err) {
-  console.error(err.stack);
+  console.error(new Date(), err.stack);
+});
+
+/* Event emitted after a message was received and parsed. */
+mailin.on('message', function (connection, data, content) {
+    console.log(new Date(), 'message connection ->', data);
+    console.log(new Date(), 'message data ->', data);
+    console.log(new Date(), 'message content ->', content);
+    /* Do something useful with the parsed message here.
+     * Use parsed message `data` directly or use raw message `content`. */
 });
 
 module.exports = mailin;
